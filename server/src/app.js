e835187d28110a91613af36865e29cf7c0e456f6
Facundo Sura <facundomesura@gmail.com>
2025-03-23 08:13:28 -0300

    peticion al server de todos los productos en la homepage

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");

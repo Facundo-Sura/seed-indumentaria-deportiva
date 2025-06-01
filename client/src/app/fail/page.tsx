eada5756b778a009c512fd412d0fd92ec22e917a
facundo sura <facundomesura@gmail.com>
2025-06-01 03:10:57 -0300

    fail page

---
+++ client/src/app/fail/page.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+const Fail = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-red-100 text-red-800 text-center p-6">
+            <h1 className="text-4xl mb-6">Compra Fallida</h1>
+            <p className="text-lg mb-8">Lamentamos que no se haya concretado tu compra. Si necesitas ayuda, por favor contáctanos.</p>
+                <Link href="/products" className="bg-red-200 text-red-800 px-6 py-3 rounded-lg text-lg transition-colors duration-300 hover:bg-red-300">
+                    Ver más productos
+                </Link>
+        </div>
+    );
+};
+
+export default Fail;
